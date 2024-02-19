a8bc44e58741f3f1b3838e1831b1928cb05a990e
Anderson Moura <andersonmlima2@gmail.com>
2024-02-19 17:47:29 -0300

    Storyboard concluido

---
+++ KiwiKoin/KiwiKoin/View/KiwiKoinController.swift
@@ -0,0 +1,19 @@
+//
+//  KiwiKoinController.swift
+//  KiwiKoin
+//
+//  Created by Anderson Moura on 19/02/24.
+//
+
+import UIKit
+import iOSDropDown
+
+class KiwiKoinController: UIViewController {
+    
+    @IBOutlet weak var dropDownTo: DropDown!
+    @IBOutlet weak var dropDownFrom: DropDown!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
